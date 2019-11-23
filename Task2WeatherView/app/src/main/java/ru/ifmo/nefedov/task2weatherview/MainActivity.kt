d7ba242b6d6b0a93b97a69230879eac46099fbdf
Dmitrii Nefedov <nefedov270399@yandex.ru>
2019-11-23 14:14:56 +0300

    Task #2: Clean all

--- Task2WeatherView/app/src/main/java/ru/ifmo/nefedov/task2weatherview/MainActivity.kt
+++ Task2WeatherView/app/src/main/java/ru/ifmo/nefedov/task2weatherview/MainActivity.kt
@@ -1,16 +1,12 @@
 package ru.ifmo.nefedov.task2weatherview
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.today_extra_block.view.*
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        wind.description.text = "Hi!"
     }
 }
