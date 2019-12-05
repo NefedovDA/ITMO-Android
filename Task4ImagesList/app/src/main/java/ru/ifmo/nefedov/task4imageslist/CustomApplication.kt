1065089520fb260b362b9df7b660c977264bf4d2
Dmitrii Nefedov <nefedov270399@yandex.ru>
2019-12-06 01:00:25 +0300

    Task #4: New location of unsplash keys

--- Task4ImagesList/app/src/main/java/ru/ifmo/nefedov/task4imageslist/CustomApplication.kt
+++ Task4ImagesList/app/src/main/java/ru/ifmo/nefedov/task4imageslist/CustomApplication.kt
@@ -2,17 +2,16 @@ package ru.ifmo.nefedov.task4imageslist
 
 import android.app.Application
 import com.unsplash.pickerandroid.photopicker.UnsplashPhotoPicker
-import ru.ifmo.nefedov.task4imageslist.model.UnsplashKeysBundle.ACCESS_KEY
-import ru.ifmo.nefedov.task4imageslist.model.UnsplashKeysBundle.SECRET_KEY
-import ru.ifmo.nefedov.task4imageslist.model.getUnsplashKey
+import ru.ifmo.nefedov.task4imageslist.model.UnsplashKeys.ACCESS_KEY
+import ru.ifmo.nefedov.task4imageslist.model.UnsplashKeys.SECRET_KEY
 
 class CustomApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         UnsplashPhotoPicker.init(
             this,
-            getUnsplashKey(ACCESS_KEY),
-            getUnsplashKey(SECRET_KEY)
+            ACCESS_KEY,
+            SECRET_KEY
         ).setLoggingEnabled(true)
     }
 }
---
+++ Task4ImagesList/app/src/main/java/ru/ifmo/nefedov/task4imageslist/model/UnsplashKeys.kt
@@ -0,0 +1,6 @@
+package ru.ifmo.nefedov.task4imageslist.model
+
+object UnsplashKeys {
+    const val ACCESS_KEY: String = "?"
+    const val SECRET_KEY: String = "?"
+}
