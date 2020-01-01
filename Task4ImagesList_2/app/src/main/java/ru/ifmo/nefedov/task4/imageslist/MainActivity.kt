21188f616cb886bd4e0c47655a5c0200e1947903
Dmitrii Nefedov <nefedov270399@yandex.ru>
2020-01-01 16:08:01 +0300

    Task4/2: Add show ok dialog

--- Task4ImagesList_2/app/src/main/java/ru/ifmo/nefedov/task4/imageslist/MainActivity.kt
+++ Task4ImagesList_2/app/src/main/java/ru/ifmo/nefedov/task4/imageslist/MainActivity.kt
@@ -1,12 +1,25 @@
 package ru.ifmo.nefedov.task4.imageslist
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.AlertDialog
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    private fun showOkDialog(message: String) {
+        val builder = AlertDialog.Builder(this@MainActivity)
+            .apply {
+                setTitle(R.string.no_internet_connection)
+                setMessage(message)
+                setCancelable(false)
+                setNegativeButton("OK") { dialog, _ -> dialog.cancel() }
+            }
+        val alert = builder.create()
+        alert.show()
     }
 }
