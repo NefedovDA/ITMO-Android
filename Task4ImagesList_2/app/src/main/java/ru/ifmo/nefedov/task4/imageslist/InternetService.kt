33ab8a8996638da2885f5b8c74e47285b41b53e5
Dmitrii Nefedov <nefedov270399@yandex.ru>
2020-01-02 22:19:09 +0300

    Task4/2: Add InternetService skeleton

---
+++ Task4ImagesList_2/app/src/main/java/ru/ifmo/nefedov/task4/imageslist/InternetService.kt
@@ -0,0 +1,67 @@
+package ru.ifmo.nefedov.task4.imageslist
+
+import android.app.IntentService
+import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
+import android.util.Log
+
+class InternetService : IntentService("ru.ifmo.nefedov.task4.imageslist.InternetService") {
+    override fun onHandleIntent(intent: Intent?) {
+        if (intent == null) {
+            Log.e(LOG_KEY, "Intent is null")
+            return
+        }
+
+        val mode = intent.getStringExtra(DOWNLOAD_MODE_KEY)
+        when (mode) {
+            DOWNLOAD_PREVIEW_LIST -> downloadPreviewList()
+            DOWNLOAD_FULLSCREEN -> {
+                val url = intent.getStringExtra(DOWNLOAD_EXTRA_KEY)
+                if (url == null) {
+                    Log.e(LOG_KEY, "Url fo downloading fullscreen is null")
+                    return
+                }
+                downloadFullscreen(url)
+            }
+            else -> Log.e(LOG_KEY, "Mode is null or has undefined value")
+        }
+    }
+
+    private fun downloadSingleImage(url: String): Bitmap {
+        TODO()
+    }
+
+    private fun downloadFullscreen(url: String) {
+        // TODO
+    }
+
+    private fun downloadPreviewList() {
+        // TODO
+    }
+
+    companion object {
+        private const val LOG_KEY = "InternetService"
+
+        private const val DOWNLOAD_MODE_KEY = "download_mode"
+        private const val DOWNLOAD_PREVIEW_LIST = "download_list"
+        private const val DOWNLOAD_FULLSCREEN = "download_fullscreen"
+
+        private const val DOWNLOAD_EXTRA_KEY = "download_extra"
+
+        private fun runDownloading(context: Context, mode: String, extra: String? = null) {
+            val intent = Intent(context, InternetService::class.java).apply {
+                putExtra(DOWNLOAD_MODE_KEY, mode)
+                extra?.let { putExtra(DOWNLOAD_EXTRA_KEY, it) }
+            }
+
+            context.startService(intent)
+        }
+
+        fun downloadPreviewList(context: Context) = runDownloading(context, DOWNLOAD_PREVIEW_LIST)
+        fun downloadFullscreen(context: Context, url: String) =
+            runDownloading(context, DOWNLOAD_FULLSCREEN, url)
+
+        const val RESULT_KEY = "result_value"
+    }
+}
