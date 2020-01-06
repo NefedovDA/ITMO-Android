37b8f7a3a19c1c0866fb3f1d9f007c48b09ca3e8
Dmitrii Nefedov <nefedov270399@yandex.ru>
2020-01-06 03:01:50 +0300

    Task#5: Add layouts & add some logic to MainActivity

--- Task5Navigation/app/src/main/java/ru/ifmo/nefedov/task5/navigation/MainActivity.kt
+++ Task5Navigation/app/src/main/java/ru/ifmo/nefedov/task5/navigation/MainActivity.kt
@@ -1,12 +1,44 @@
 package ru.ifmo.nefedov.task5.navigation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var currentFragmentTag: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        main_bottom_navigation?.setOnNavigationItemSelectedListener {
+            selectTab("${it.itemId}")
+            true
+        }
+
+        main_navigation?.setNavigationItemSelectedListener {
+            selectTab("${it.itemId}")
+            true
+        }
+    }
+
+    private fun selectTab(fragmentTag: String) {
+        val fragment = supportFragmentManager.findFragmentByTag(fragmentTag) ?: TabFragment()
+
+        val transaction = supportFragmentManager.beginTransaction()
+
+        supportFragmentManager.findFragmentByTag(currentFragmentTag)?.let {
+            transaction.hide(it)
+        }
+
+        if (fragment.isAdded) {
+            transaction.show(fragment)
+        } else {
+            transaction.add(R.id.main_fragmentContainer, fragment, fragmentTag)
+        }
+
+        currentFragmentTag = fragmentTag
+        transaction.commit()
     }
 }
---
+++ Task5Navigation/app/src/main/java/ru/ifmo/nefedov/task5/navigation/TabFragment.kt
@@ -0,0 +1,6 @@
+package ru.ifmo.nefedov.task5.navigation
+
+import androidx.fragment.app.Fragment
+
+class TabFragment : Fragment() {
+}
