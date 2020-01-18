5778f5369bf6e107589bcc8dcfac47c974d2014c
Dmitrii Nefedov <nefedov270399@yandex.ru>
2020-01-18 19:22:10 +0300

    task#7: Task #2 version

---
+++ Task7WeatherWeb/app/src/main/java/ru/ifmo/nefedov/task7/weather/web/items/day/DayViewHolder.kt
@@ -0,0 +1,13 @@
+package ru.ifmo.nefedov.task7.weather.web.items.day
+
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.week_day.view.*
+
+class DayViewHolder(val root: View) : RecyclerView.ViewHolder(root) {
+    val descriptionText: TextView = root.day_description
+    val image: ImageView = root.day_image
+    val tempText: TextView = root.day_temp
+}
