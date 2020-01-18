67b21228d57a35969b2e4cda68fc9106a25a3a3b
Dmitrii Nefedov <nefedov270399@yandex.ru>
2020-01-18 14:03:10 +0300

    task#5: fix selection

--- Task5Navigation/app/src/main/java/ru/ifmo/nefedov/task5/navigation/MainActivity.kt
+++ Task5Navigation/app/src/main/java/ru/ifmo/nefedov/task5/navigation/MainActivity.kt
@@ -2,7 +2,9 @@ package ru.ifmo.nefedov.task5.navigation
 
 import android.os.Bundle
 import android.util.Log
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.forEach
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -17,21 +19,25 @@ class MainActivity : AppCompatActivity() {
             selectTab(R.id.navigation_home)
         } else {
             currentFragmentTag = savedInstanceState.getString(CURRENT_FRAGMENT_TAG_KEY)
+            val menuItem = (main_navigation?.menu ?: main_bottom_navigation?.menu)!!
+                .findItem(currentFragmentTag!!.toInt())
+            menuItem.isChecked = true
         }
 
-        main_bottom_navigation?.setOnNavigationItemSelectedListener {
-            selectTab(it.itemId)
-            true
-        }
-
-        main_navigation?.setNavigationItemSelectedListener {
-            selectTab(it.itemId)
-            true
-        }
+        main_bottom_navigation?.setOnNavigationItemSelectedListener(::selectItem)
+        main_navigation?.setNavigationItemSelectedListener(::selectItem)
     }
 
-    private fun selectTab(tabInd: Int) {
-        val fragmentTag = "$tabInd"
+    private fun selectItem(menuItem: MenuItem): Boolean {
+        selectTab(menuItem.itemId)
+        main_navigation?.menu?.forEach { it.isChecked = false }
+        menuItem.isChecked = true
+        return true
+    }
+
+    private fun selectTab(tabInd: Int) = selectTab("$tabInd")
+
+    private fun selectTab(fragmentTag: String) {
         val fragment = supportFragmentManager.findFragmentByTag(fragmentTag) ?: TabFragment()
 
         val transaction = supportFragmentManager.beginTransaction()
