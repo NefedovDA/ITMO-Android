d5e004411559b11294c19e5413db406b7dff47c4
Dmitrii Nefedov <nefedov270399@yandex.ru>
2020-01-22 13:53:30 +0300

    Task#8: Add empty project

---
+++ Task8WeatherBD/app/src/main/java/ru/ifmo/nefedov/task8/weather/bd/MainActivity.kt
@@ -0,0 +1,12 @@
+package ru.ifmo.nefedov.task8.weather.bd
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
