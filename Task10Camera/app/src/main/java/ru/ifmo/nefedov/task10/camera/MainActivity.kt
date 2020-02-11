baa9f478a8cb088417aaed5b77850002b12ffc9d
dmitry.nefedov <nefedov270399@yandex.ru>
2020-02-11 20:46:32 +0300

    Task#10: Add empty project

---
+++ Task10Camera/app/src/main/java/ru/ifmo/nefedov/task10/camera/MainActivity.kt
@@ -0,0 +1,12 @@
+package ru.ifmo.nefedov.task10.camera
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
