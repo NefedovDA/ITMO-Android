2c101ad4d09241caeae55c5a84f0894e63ad9aba
dmitry.nefedov <nefedov270399@yandex.ru>
2020-02-12 20:11:15 +0300

    Task#10: Add full task

--- Task10Camera/app/src/main/java/ru/ifmo/nefedov/task10/camera/MainActivity.kt
+++ Task10Camera/app/src/main/java/ru/ifmo/nefedov/task10/camera/MainActivity.kt
@@ -1,12 +1,144 @@
 package ru.ifmo.nefedov.task10.camera
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+import android.content.pm.PackageManager
+import android.graphics.Matrix
+import android.os.Bundle
+import android.util.Size
+import android.view.Surface
+import android.view.TextureView
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.*
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.LifecycleOwner
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
+import java.util.concurrent.Executors
+
+
+class MainActivity : AppCompatActivity(), LifecycleOwner {
+
+    private val executor = Executors.newSingleThreadExecutor()
+    private lateinit var textureView: TextureView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textureView = view_finder
+
+        if (allPermissionsGranted()) {
+            textureView.post { startCamera() }
+        } else {
+            ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
+        }
+
+        textureView.addOnLayoutChangeListener { _, _, _, _, _, _, _, _, _ ->
+            updateTransform()
+        }
+    }
+
+    private fun startCamera() {
+        val previewConfig = PreviewConfig.Builder().apply {
+            setTargetResolution(Size(640, 480))
+        }.build()
+
+        val preview = Preview(previewConfig)
+
+        preview.setOnPreviewOutputUpdateListener {
+            val parent = textureView.parent as ViewGroup
+            parent.removeView(textureView)
+            parent.addView(textureView, 0)
+
+            textureView.surfaceTexture = it.surfaceTexture
+            updateTransform()
+        }
+
+        val imageCaptureConfig = ImageCaptureConfig.Builder().apply {
+            setCaptureMode(ImageCapture.CaptureMode.MIN_LATENCY)
+        }.build()
+
+        val imageCapture = ImageCapture(imageCaptureConfig)
+        textureView.setOnClickListener {
+            val file = File(externalMediaDirs.first(), "${System.currentTimeMillis()}.jpg")
+
+            imageCapture.takePicture(file, executor,
+                object : ImageCapture.OnImageSavedListener {
+                    override fun onImageSaved(file: File) {
+                        val msg = "Photo capture succeeded: ${file.absolutePath}"
+                        textureView.post {
+                            Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+                        }
+                    }
+
+                    override fun onError(
+                        imageCaptureError: ImageCapture.ImageCaptureError,
+                        message: String,
+                        cause: Throwable?
+                    ) {
+                        val msg = "Photo capture failed: $message"
+                        textureView.post {
+                            Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+                        }
+                    }
+
+                })
+        }
+
+        CameraX.bindToLifecycle(this, preview, imageCapture)
+    }
+
+    private fun updateTransform() {
+        val matrix = Matrix()
+
+        val w = textureView.measuredWidth
+        val h = textureView.measuredHeight
+
+        val centerX = w / 2f
+        val centerY = h / 2f
+
+        val rotationDegrees = when (textureView.display.rotation) {
+            Surface.ROTATION_0 -> 0
+            Surface.ROTATION_90 -> 90
+            Surface.ROTATION_180 -> 180
+            Surface.ROTATION_270 -> 270
+            else -> return
+        }
+
+        matrix.postRotate(-rotationDegrees.toFloat(), centerX, centerY)
+        textureView.setTransform(matrix)
+    }
+
+    private fun allPermissionsGranted(): Boolean =
+        REQUIRED_PERMISSIONS.all {
+            ContextCompat.checkSelfPermission(this, it) == PackageManager.PERMISSION_GRANTED
+        }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                startCamera()
+            } else {
+                Toast.makeText(
+                    this,
+                    "Permissions not granted by the user.",
+                    Toast.LENGTH_SHORT
+                ).show()
+                finish()
+            }
+        }
+    }
+
+    companion object {
+        const val REQUEST_CODE_PERMISSIONS = 101
+        val REQUIRED_PERMISSIONS =
+            arrayOf("android.permission.CAMERA", "android.permission.WRITE_EXTERNAL_STORAGE")
     }
 }
